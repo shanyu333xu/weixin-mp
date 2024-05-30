157c4c8c65e00f88e6250c3c2a281a752d91be01
Xingwei <912254883@qq.com>
2024-05-30 15:10:38 +0800

    feat: 自选页面-静态结构

--- src/pages/favorites/favorites.vue
+++ src/pages/favorites/favorites.vue
@@ -2,17 +2,249 @@
 {
   layout: 'default',
   style: {
-    navigationBarTitleText: '自选',
+    navigationBarTitleText: '同花顺自选股',
   },
 }
 </route>
 
 <template>
-  <view class="">自选</view>
+  <!-- 搜索框,其实是跳转到搜索页面 -->
+  <view>
+    <navigator url="/pages/" open-type="navigate" hover-class="navigator-hover">
+      <image src="" mode="scaleToFill" />
+      <text>搜股票名称/股票代码</text>
+    </navigator>
+  </view>
+  <!-- 消息栏 -->
+  <view>消息栏</view>
+  <!-- 自选列表 -->
+  <view class="stock-list">
+    <!-- 表头 -->
+    <view class="stock-header">表头</view>
+    <!-- 内容 -->
+    <view class="stock-item" v-for="(item, index) in stocks" :key="index">
+      <navigator
+        :url="`/pages/stock-detail?code=${item.code}`"
+        open-type="navigate"
+        hover-class="navigator-hover"
+      >
+        <text>{{ item.name }}</text>
+        <text>{{ item.code }}</text>
+        <text>{{ item.price }}</text>
+        <text>{{ item.change }}</text>
+      </navigator>
+    </view>
+  </view>
 </template>
 
 <script lang="ts" setup>
-//
+// 测试数据
+const stocks = [
+  {
+    name: '股票A',
+    code: '000001',
+    price: '10.00',
+    change: '+0.5%',
+  },
+  {
+    name: '股票B',
+    code: '000002',
+    price: '20.50',
+    change: '-1.2%',
+  },
+  {
+    name: '股票C',
+    code: '000003',
+    price: '15.75',
+    change: '+2.0%',
+  },
+  {
+    name: '股票D',
+    code: '000004',
+    price: '10.00',
+    change: '+0.5%',
+  },
+  {
+    name: '股票F',
+    code: '000005',
+    price: '21.50',
+    change: '-1.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+  {
+    name: '股票G',
+    code: '000006',
+    price: '15.75',
+    change: '+2.8%',
+  },
+]
 </script>
 
 <style lang="scss" scoped>
