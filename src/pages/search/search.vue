60eed1823fa3a9432c37222e546154e29fa04a5d
Xingwei <912254883@qq.com>
2024-06-04 18:22:03 +0800

    feat: 搜索页面-搜索框

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -8,13 +8,41 @@
 </route>
 
 <template>
-  <view class="">搜索页</view>
+  <!-- 搜索输入框 -->
+  <view class="search-container">
+    <input
+      class="search-input"
+      type="text"
+      placeholder="请输入股票名称/股票代码"
+      confirm-type="search"
+      @confirm="onSearchConfirm"
+    />
+  </view>
 </template>
 
 <script lang="ts" setup>
-//
+const searchText = ref('')
+const onSearchConfirm: UniHelper.InputOnConfirm = (event) => {
+  searchText.value = event.detail.value
+  console.log(`搜索内容： ${searchText.value}`)
+}
 </script>
 
 <style lang="scss" scoped>
-//
+.search-container {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #000000;
+  border-radius: 20px;
+}
+
+.search-input {
+  width: 100%;
+  padding: 0px 80px 0px 80px;
+  font-size: 16px;
+  border: none;
+  border-radius: 20px;
+}
 </style>
